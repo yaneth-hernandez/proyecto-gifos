e405261c3feb8d6efd5e2010244a4c7f9efcd30d
yaneth-hernandez <yahecaceres@gmail.com>
2020-05-09 23:02:59 -0500

    despliegue de barra cambiar tema, cambio de tema e icono con JS. Prueba en html para despliega de botones

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
-function vermasOpt1(ver) {
-    alert('mirar gif');
+function themeButtonDopdown() {
+    var objectSelector = document.querySelector("#contenedor-menu").style.visibility;
+    if (objectSelector == "" || objectSelector == "hidden") {
+        document.querySelector("#contenedor-menu").style.visibility = "visible";
+    } else {
+        document.querySelector("#contenedor-menu").style.visibility = "hidden";
+    }
+}
+
+function themeSailorNight() {
+    document.querySelector("#style-theme-rel").href = "/css/style-theme2.css";
+    document.querySelector("#header-logo").src = "img/gifOF_logo_dark.png";
+    themeButtonDopdown();
+}
+
+function themeSailorDay() {
+    document.querySelector("#style-theme-rel").href = "/css/style.css";
+    document.querySelector("#header-logo").src = "img/gifOF_logo.png";
+    themeButtonDopdown();
 }
