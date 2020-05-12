79292aa603555e41685d2e60fc73a471d3654c94
yaneth-hernandez <yahecaceres@gmail.com>
2020-05-11 23:41:22 -0500

    maquetado de página crear gifos day y night

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//página principal
+
 function themeButtonDopdown() {
     var objectSelector = document.querySelector("#contenedor-menu").style.visibility;
     if (objectSelector == "" || objectSelector == "hidden") {
@@ -16,5 +18,29 @@ function themeSailorNight() {
 function themeSailorDay() {
     document.querySelector("#style-theme-rel").href = "/css/style.css";
     document.querySelector("#header-logo").src = "img/gifOF_logo.png";
+    themeButtonDopdown();
+}
+
+function opcionesBusqueda() {
+
+    var objectSelector = document.querySelector(".resultado-sugerido").style.visibility;
+    if (objectSelector == "" || objectSelector == "hidden") {
+        document.querySelector(".resultado-sugerido").style.visibility = "visible";
+    } else {
+        document.querySelector(".resultado-sugerido").style.visibility = "hidden";
+    }
+}
+
+//crear gifos
+
+function themeSailorNightCrGifo() {
+    document.querySelector("#style-theme-crear-gifo").href = "css/stylecreargifo-theme2.css";
+    document.querySelector("#header-logo-arrow").src = "img/gifOF_logo_dark.png";
+    themeButtonDopdown();
+}
+
+function themeSailorDayCrGifo() {
+    document.querySelector("#style-theme-crear-gifo").href = "css/stylecreargifo.css";
+    document.querySelector("#header-logo-arrow").src = "img/gifOF_logo.png";
     themeButtonDopdown();
 }
